115b300d7d80743035c778368fa5f4aedc848525
Carlos <contact@carloscolin.me>
2021-10-31 08:38:51 -0600

    Initial Pokemon Page commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Who is this Pokemon?</h1>
+
+  <!-- TODO: Picture -->
+
+  <!-- TODO: Opciones -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
