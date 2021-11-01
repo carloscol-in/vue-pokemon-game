00acbdc1e122d4b47f965d5908d7d98315cab829
Carlos <contact@carloscolin.me>
2021-11-01 01:04:43 -0600

    working with component reloading after success

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,9 +2,9 @@
 
   <h1>Who is this Pokemon?</h1>
 
-  <PokemonPicture :pokemonId="pokemon_id" :showPokemon="true" />
+  <PokemonPicture :pokemonId="pokemon_id" :showPokemon="show_pokemon" />
 
-  <PokemonOptions />
+  <PokemonOptions :options="pokemon_options" :correct_pokemon="pokemon_id" :doShowPokemon="doShowPokemon" :loadComponent="this.loadComponent" />
 
 </template>
 
@@ -12,15 +12,44 @@
 import PokemonOptions from '@/components/PokemonOptions.vue'
 import PokemonPicture from '@/components/PokemonPicture.vue'
 
+import getPokemonOptions from '@/helpers/getPokemonOptions.js'
+
 export default {
   name: 'PokemonPage',
+
   components: {
     PokemonPicture,
     PokemonOptions,
   },
+
+  methods: {
+
+    async selectPokemon() {
+      this.pokemon_id = this.pokemon_options[Math.floor(Math.random()*this.pokemon_options.length)].id
+      console.log(this.pokemon_id);
+    },
+
+    doShowPokemon() {
+      this.show_pokemon = true
+    },
+
+    async loadComponent() {
+      this.show_pokemon = false 
+      this.pokemon_options = await getPokemonOptions()
+      await this.selectPokemon()
+    },
+  },
+
+  created() {
+    this.loadComponent()
+  },
+
   data() {
     return {
-      pokemon_id: 4,
+      amount_of_options: 4,
+      pokemon_id: 0,
+      pokemon_options: [],
+      show_pokemon: false,
     }
   }
 }
